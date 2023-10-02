e564e55cd580382f8a5a3c6e6b7978155d0d5abd
Arugaf <arug113@gmail.com>
2023-10-02 14:06:42 +0300

    end game scene

---
+++ Assets/Scripts/UI/EndMenu.cs
@@ -0,0 +1,34 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace UI {
+    public class EndMenu : MonoBehaviour {
+        private TextMeshProUGUI _gameOverTitle;
+        private GameStateManager _gameStateManager;
+
+        private void Start() {
+            _gameStateManager = FindAnyObjectByType<GameStateManager>();
+            _gameOverTitle = GameObject.FindGameObjectWithTag("GameOverTitle").GetComponent<TextMeshProUGUI>();
+
+            if (!_gameStateManager || !_gameOverTitle) return;
+            
+            _gameOverTitle.text.Replace("${placeholder}",
+                _gameStateManager.currentGameStatus == GameStateManager.GameStatus.Lose
+                    ? "Вы проиграли!"
+                    : "Вы Победили!");
+        }
+
+        public void LoadMainMenu() {
+            _gameStateManager.LoadMenu();
+        }
+
+        public void Exit() {
+            _gameStateManager.Exit();
+        }
+
+        public void ShowStatistics() {
+            // todo
+        }
+    }
+}
